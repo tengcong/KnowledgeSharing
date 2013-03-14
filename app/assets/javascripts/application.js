9a016d56d4529ecfcce8a264b972f08d8707b181
congteng <congteng45@gmail.com>
2013-03-14 22:05:38 +0800

    add selector in list page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -35,23 +35,64 @@ function recover(obj){
   }, 500)
 }
 
+var cursor = -1;
 commandLine.bind('keydown', function(env){
-  // if(env.which == 13){
-  //   var command = commandLine.val();
-  //   var match_nums = command.match(/:(\d+)/);
-  //   var pageNum = 0;
-  //   if(match_nums){
-  //     pageNum = match_nums[1] - 1;
-  //   }
-  //   console.log(pageNum);
-
-  //   // window.location = $('a.title')[pageNum];
-  // }
   if (env.which === 38) {
     var list = $('a.title');
-    console.log(list);
+    cursor = getPre(cursor >= (list.length) ? (list.length) : cursor, list.length);
+    var selector = list.get(cursor);
+    recoverAllBackColor(list)
+    setBackColor(selector);
 
   } else if (env.which === 40) {
-    alert('down');
+    var list = $('a.title');
+    cursor = getNext(cursor, list.length);
+    var selector = list.get(cursor);
+    recoverAllBackColor(list);
+    setBackColor(selector);
+
+  } else if (env.which === 27){
+    var list = $('a.title');
+    cursor = -1;
+
+    recoverAllBackColor(list)
+  }else if(env.which === 13){
+    var list = $('a.title');
+    var selector = list.get(cursor);
+    if(list && selector){
+      var path = $(selector).attr('href');
+      commandLine.val('');
+      window.location = path;
+    }
+    var command = commandLine.val();
+    if(command === 'home'){
+      window.location = '/';
+    }else if(command === 'share'){
+      commandLine.val('');
+      window.location = '/#sharing';
+    }
   }
 })
+
+function getPre(curr, size){
+  if(curr <= 0){
+    return size - 1;
+  }else {
+    return curr - 1;
+  }
+}
+function getNext(curr, size){
+  if(curr < size - 1){
+    return curr + 1;
+  }else {
+    return 0;
+  }
+}
+function recoverAllBackColor(list){
+  _.each(list, function(e){
+    $(e).parents('.cell').css('background-color', 'white');
+  })
+}
+function setBackColor(selector){
+  $(selector).parents('.cell').css('background-color', '#D5F3F5');
+}
